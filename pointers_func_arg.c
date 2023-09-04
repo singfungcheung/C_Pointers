8b123e793e472fcb52fb21468cd2ec075aac3119
Sing Fung Cheung <singfung.cheung@gmail.com>
2023-09-04 14:02:36 -0400

    added comments

--- pointers_func_arg.c
+++ pointers_func_arg.c
@@ -8,7 +8,7 @@ void Increment(int x)
 // What's happening here is this is "Call to value" so if a main function calls this nothing will happen to the value passed into the function (local var).
 */
 
-// Instead, pass the address
+// Instead, pass the address. Advantages of this are that it saves a lot of memory because it doesn't create copies, but reference to it. 
 void increment(int *p)
 {
     *p = *p + 1;
